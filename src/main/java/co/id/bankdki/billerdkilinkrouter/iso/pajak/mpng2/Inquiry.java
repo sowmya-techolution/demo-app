b2c13a408c46c309a2f96672a5ed6e100878aaf1
Yuanita Herawati <yuanita.herawati@gmail.com>
2019-05-03 15:18:19 +0700

    delete code m.set 105 and m.set 106

--- src/main/java/co/id/bankdki/billerdkilinkrouter/iso/pajak/mpng2/Inquiry.java
+++ src/main/java/co/id/bankdki/billerdkilinkrouter/iso/pajak/mpng2/Inquiry.java
@@ -106,8 +106,6 @@ import java.text.DecimalFormatSymbols;
                      m.set(59,resp.getString(59));
                      m.set(61,resp.getString(61));
                      m.set(63,resp.getString(63));
-                     m.set(105,"");
-                     m.set(106,"");
                  }
                  m.set(39,resp.getString(39));
                  m.set(61,resp.getString(61));
